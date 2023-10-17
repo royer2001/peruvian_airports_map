101ae64364f88dce2957f87afbb06c54f2e40935
royeraf <royercfm@gmail.com>
2023-10-16 22:34:27 -0500

    fix: add leaflet configuration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="height: 600px; width: 500px;">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <AriportsMap msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AriportsMap from './components/AriportsMap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AriportsMap
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import * as Vue3Leaflet from 'vue3-leaflet';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// Registrar todos los componentes de vue3-leaflet
+for (const componentKey in Vue3Leaflet) {
+  app.component(componentKey, Vue3Leaflet[componentKey]);
+}
+
+app.mount('#app');
