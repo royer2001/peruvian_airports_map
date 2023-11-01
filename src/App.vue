77f2fd7ca1c24f2deba5a3d53943c2d94dac2e38
royeraf <royercfm@gmail.com>
2023-10-31 21:24:54 -0500

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <AriportsMap msg="Welcome to Your Vue.js App"/>
+    <AriportsMap msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
--- src/components/AriportsMap.vue
+++ src/components/AriportsMap.vue
@@ -1,56 +1,62 @@
 <template>
-<div class="container">
+  <div class="container">
+    <div class="my-4 p-5 bg-primary text-white rounded">
+      <h1>{{ headerPresentation.title }}</h1>
+      <p>{{ headerPresentation.description }}</p>
+    </div>
 
-  <div class="row">
-    <div class="col-6">
-      <div>
-        <h3 class="text-center">Peruvian Airports</h3>
-        <l-map :options="mapOptions" :zoom="5" :center="[-9.1900, -75.0152]">
-          <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
-          
-          <!-- Agregar marcadores -->
-          <!-- <l-marker :lat-lng="[-12.0464, -77.0428]">
+    <div class="row mb-4">
+      <div class="col-6">
+        <div style="height: 500px;">
+          <h3 class="text-center">Peruvian Airports Map</h3>
+          <l-map :options="mapOptions" :zoom="5" :center="[-9.1900, -75.0152]">
+            <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
+
+            <!-- Agregar marcadores -->
+            <!-- <l-marker :lat-lng="[-12.0464, -77.0428]">
             <l-popup>
               Este es el mensaje que se muestra cuando haces clic en el marcador en Juliaca.
             </l-popup>
           </l-marker>  -->
-          <!-- <l-marker :lat-lng="[13.6918, -89.2248]"></l-marker> -->
+            <!-- <l-marker :lat-lng="[13.6918, -89.2248]"></l-marker> -->
 
-          <l-marker :lat-lng="[-15.470319163106165, -70.15666099196471]">
-            <l-popup>
-              JULIACA AIRPORT: INCA MANCO CAPAC AIRPORT
-            </l-popup>
-          </l-marker>
+            <l-marker :lat-lng="[-15.470319163106165, -70.15666099196471]"
+              @click="handleMarkerClick('JULIACA AIRPORT: INCA MANCO CAPAC AIRPORT')">
+              <l-popup>
+                JULIACA AIRPORT: INCA MANCO CAPAC AIRPORT
+              </l-popup>
+            </l-marker>
 
-          <!-- Agrega más marcadores según sea necesario -->
-        </l-map>
+            <!-- Agrega más marcadores según sea necesario -->
+          </l-map>
+        </div>
+
       </div>
-      
-      <div class="row">
-        <div class="col-4">
-          <button class="btn btn-primary" @click="fetchData()">axios test</button>
+      <div class="col-6">
+        <div>
+          <div class="row">
+            <div class="col-4">
+              <button class="btn btn-primary" @click="resetInfo()">RESET</button>
+            </div>
+            <div class="col-4">
+              <button class="btn btn-info" @click="fetchData()">OPEN IN MAPS</button>
+            </div>
+            <div class="col-4">
+              <button class="btn btn-danger" @click="fetchData()">axios test</button>
+            </div>
+
+          </div>
+          <h3>Information</h3>
+          <section style="background: lightcyan; padding: 2rem;">
+            <p v-if="selectedAirportInfo !== ''">{{ selectedAirportInfo }}</p>
+            <img v-else src="" alt="no selected optin info">
+          </section>
+
         </div>
-        <div class="col-4">
-          <button class="btn btn-info" @click="fetchData()">axios test</button>
-        </div>
-        <div class="col-4">
-          <button class="btn btn-danger" @click="fetchData()">axios test</button>
-        </div>
-        
       </div>
-      
     </div>
-    <div class="col-6">
-      <div>
-        <h3>Information</h3>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, culpa facilis! Unde omnis fuga corporis dolores cupiditate, enim assumenda alias id aspernatur laudantium neque quibusdam eaque blanditiis veniam est repellat.
-        </p>
-      </div>
-    </div>
+
   </div>
-
-</div>
 </template>
 
 <script>
@@ -59,7 +65,7 @@ import axios from 'axios';
 
 
 export default {
-  
+
   data() {
     return {
       mapOptions: {
@@ -67,24 +73,41 @@ export default {
         zoomControl: false, // Desactiva el control de zoom
         scrollWheelZoom: false,
       },
+
+      headerPresentation: {
+        title: 'PERUVIAN AIRPORTS',
+        description: 'Learn about Peru airports.',
+      },
+
+      selectedAirportInfo: '',
+
     }
   },
-  methods:{
+  methods: {
+
+    handleMarkerClick(airportInfo) {
+      this.selectedAirportInfo = airportInfo;
+    },
+
     disableScroll(event) {
       // Previene el evento de desplazamiento cuando el mouse está sobre el mapa
       event.preventDefault();
     },
 
+    resetInfo(){
+      this.selectedAirportInfo = "";
+    },
+
     fetchData() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        // Maneja la respuesta aquí
-        console.log(response.data); // Ejemplo: Imprimir la respuesta en la consola
-      })
-      .catch(error => {
-        // Maneja el error aquí
-        console.error(error);
-      });
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+          // Maneja la respuesta aquí
+          console.log(response.data); // Ejemplo: Imprimir la respuesta en la consola
+        })
+        .catch(error => {
+          // Maneja el error aquí
+          console.error(error);
+        });
     }
   }
 };
