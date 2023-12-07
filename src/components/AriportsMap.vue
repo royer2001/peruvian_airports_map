cf99e94ee1c7d2361fe4473ab539c825c3393058
Royer <royer@localhost.localdomain>
2023-12-07 00:07:18 -0500

    fix: some styles problems in airports map

--- src/components/AriportsMap.vue
+++ src/components/AriportsMap.vue
@@ -1,94 +1,96 @@
 <template>
-  <div class="container-fluid">
-    <div class="my-2 p-5 bg-primary text-white rounded">
+  <div>
+    <div class="m-2 p-5 bg-primary text-white rounded">
       <h1>{{ headerPresentation.title }}</h1>
       <p>{{ headerPresentation.description }}</p>
     </div>
 
-    <div class="row mb-4">
-      <div class="col-6">
-        <div style="height: auto; background: #F4F1E9;">
-          <h3 class="text-center">Peruvian Airports Map</h3>
-          <div class="map-content">
-            <l-map :options="mapOptions" :zoom="zoom" :center="[latitude, longitud]">
-              <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
+    <div class="container-fluid">
+      <div class="row mb-4">
+        <div class="col-6">
+          <div style="height: auto; background: #F4F1E9;">
+            <h3 class="text-center">Peruvian Airports Map</h3>
+            <div class="map-content">
+              <l-map :options="mapOptions" :zoom="zoom" :center="[latitude, longitud]">
+                <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
 
-              <!-- Agregar marcadores -->
-              <!-- <l-marker :lat-lng="[-12.0464, -77.0428]">
+                <!-- Agregar marcadores -->
+                <!-- <l-marker :lat-lng="[-12.0464, -77.0428]">
               <l-popup>
                 Este es el mensaje que se muestra cuando haces clic en el marcador en Juliaca.
               </l-popup>
             </l-marker>  -->
-              <!-- <l-marker :lat-lng="[13.6918, -89.2248]"></l-marker> -->
+                <!-- <l-marker :lat-lng="[13.6918, -89.2248]"></l-marker> -->
 
-              <l-marker v-for="(airport, index) in airports" :key="index"
-                :lat-lng="[parseFloat(airport.latitude), parseFloat(airport.longitud)]"
-                @click="handleMarkerClick(airport.name)">
-                <l-popup>
-                  {{ airport.name }}
-                </l-popup>
-              </l-marker>
-              <!-- Agrega más marcadores según sea necesario -->
-            </l-map>
-          </div>
-        </div>
-
-      </div>
-      <div class="col-6">
-        <div>
-          <div class="row">
-            <div class="col-4">
-              <button class="btn btn-primary" @click="resetInfo()">RESET</button>
-            </div>
-            <div class="col-4">
-              <button class="btn btn-info" @click="toggleInfoSection()">OPEN IN MAPS</button>
-            </div>
-            <div class="col-4">
-              <button class="btn btn-danger" @click="fetchData()">axios test</button>
-            </div>
-
-          </div>
-          <section style="background: lightcyan; padding: 2rem;" v-if="showCurrentInfo">
-            <h3>Information</h3>
-            <div v-if="selectedAirportInfo !== ''">
-              <p>{{ selectedAirportInfo }}</p>
-              <l-map :options="mapOptions" :zoom="zoom" :center="[latitude, longitud]">
-                <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
+                <l-marker v-for="(airport, index) in airports" :key="index"
+                  :lat-lng="[parseFloat(airport.latitude), parseFloat(airport.longitud)]"
+                  @click="handleMarkerClick(airport.name)">
+                  <l-popup>
+                    {{ airport.name }}
+                  </l-popup>
+                </l-marker>
                 <!-- Agrega más marcadores según sea necesario -->
               </l-map>
             </div>
+          </div>
 
-            <img v-else src="" alt="no selected optin info">
-          </section>
+        </div>
+        <div class="col-6">
+          <div>
+            <div class="row">
+              <div class="col-4">
+                <button class="btn btn-primary" @click="resetInfo()">RESET</button>
+              </div>
+              <div class="col-4">
+                <button class="btn btn-info" @click="toggleInfoSection()">OPEN IN MAPS</button>
+              </div>
+              <div class="col-4">
+                <button class="btn btn-danger" @click="fetchData()">axios test</button>
+              </div>
 
-          <section v-else>
-            <ModalCreateLocation />
-          </section>
+            </div>
+            <section style="background: lightcyan; padding: 2rem;" v-if="showCurrentInfo">
+              <h3>Information</h3>
+              <div v-if="selectedAirportInfo !== ''">
+                <p>{{ selectedAirportInfo }}</p>
+                <l-map :options="mapOptions" :zoom="zoom" :center="[latitude, longitud]">
+                  <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
+                  <!-- Agrega más marcadores según sea necesario -->
+                </l-map>
+              </div>
 
+              <img v-else src="" alt="no selected optin info">
+            </section>
+
+            <section v-else>
+              <ModalCreateLocation />
+            </section>
+
+          </div>
         </div>
       </div>
     </div>
+    
+    <div class="container">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
 
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
-      style="background-color: aquamarine;">
-      <p class="col-md-4 mb-0 text-muted">&copy; 2023 conti, Inc</p>
+        <a href="/"
+          class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap" />
+          </svg>
+        </a>
 
-      <a href="/"
-        class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32">
-          <use xlink:href="#bootstrap" />
-        </svg>
-      </a>
-
-      <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-      </ul>
-    </footer>
-
+        <ul class="nav col-md-4 justify-content-end">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+        </ul>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -157,7 +159,7 @@ export default {
           name: 'Aeropuerto Internacional Inca Manco Capac',
           city: 'Juliaca'
         },
-        
+
       ]
     }
   },
@@ -211,5 +213,4 @@ export default {
   background: #c1beb7;
   border-radius: 17px;
   height: 500px;
-}
-</style>
+}</style>
